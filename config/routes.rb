11259b95a6300f15cafbaae9b29bdb984a06791c
tenzin_selise <tenzin.dorjee@selise.ch>
2018-06-02 15:30:30 +0600

    add bootstrap, controller and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'pages#home'
+  get '/recipes', to: 'recipes#index'
+  get 'recipes/new', to: 'recipes#new', as: 'new_recipes'
+  post '/recipes', to: 'recipes#create'
+  get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  patch '/recipes/:id',  to: 'recipes#update'
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+  delete 'recipes/:id', to: 'recipes#destroy'
 end
